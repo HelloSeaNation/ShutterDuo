dc5222c53b8af8e83cda02bcea7f5d19a5f6cedc
HelloSeaNation <sookann@gmail.com>
2024-05-10 09:15:02 +1200

    Commit with register form

--- src/App.tsx
+++ src/App.tsx
@@ -1,76 +1,9 @@
 import React from "react";
-import { Box, Image, Flex, Button, Text } from "@chakra-ui/react";
-import Login from "./components/Login";
+import LandingPage from "./pages/LandingPage";
 
 function App() {
   return (
-    <Box height={"100vh"} bg={"#4267CF"} margin={"auto"}>
-      <Box bg="#4267CF" p={4} color="orange">
-        <Flex justifyContent={"space-between"} w={"95%"} margin={"auto"}>
-          <Image
-            src="./shutterduologo.png"
-            alt="shutterduologo"
-            height={"50px"}
-          />
-          <Box>
-            <Button
-              color="white"
-              variant="outline"
-              borderRadius={"0"}
-              fontFamily={"Inter"}
-              fontWeight={"normal"}
-              _hover={{ color: "#4267CF", bg: "white" }}
-            >
-              LOGIN
-            </Button>
-            <Button
-              color="white"
-              variant="outline"
-              borderRadius={"0"}
-              marginLeft={"-0.4"}
-              fontFamily={"Inter"}
-              fontWeight={"normal"}
-              _hover={{ color: "#4267CF", bg: "white" }}
-            >
-              SIGN UP
-            </Button>
-          </Box>
-        </Flex>
-      </Box>
-      <Box
-        bgImage="url('./blue-whale.jpg')"
-        bgSize="cover"
-        bgPosition="center"
-        bgRepeat="no-repeat"
-        h="94vh"
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-        color="white"
-        position="relative"
-      >
-        <Box
-          textAlign="left"
-          position="absolute"
-          left="120px"
-          top="10%"
-          fontSize={"70px"}
-        >
-          <Text>Where photographer</Text>
-          <Text>send image fast and simple</Text>
-        </Box>
-      </Box>
-
-      <Box
-        textAlign="left"
-        position="absolute"
-        right="8vh"
-        top="17%"
-        fontSize={"70px"}
-      >
-        <Login />
-      </Box>
-    </Box>
+   <LandingPage />
   );
 }
 
--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,10 +1,81 @@
-import react from "react";
+import React from "react";
+import {
+  Box,
+  Flex,
+  FormControl,
+  FormLabel,
+  Input,
+  Text,
+  Button,
+} from "@chakra-ui/react";
 
-function Register() {
-    return (
-        <div>
-        <h1>Register</h1>
-        </div>
-    );
-    }
-    export default Register;    
+const Register = () => {
+  return (
+    <Box>
+      <Flex direction={"column"}>
+        <FormControl>
+          <FormLabel fontSize={"30px"} color={"white"}>
+            Join with us
+          </FormLabel>
+          <Flex direction={"row"}>
+            <Input
+              fontSize="20px"
+              type="text"
+              placeholder="First Name"
+              w={"19vh"}
+              h={"4vh"}
+              backgroundColor={"white"}
+              marginTop={"20px"}
+              marginRight={"15px"}
+            />
+            <Input
+              fontSize="20px"
+              type="text"
+              placeholder="Surname"
+              w={"19vh"}
+              h={"4vh"}
+              backgroundColor={"white"}
+              marginTop={"20px"}
+            />
+          </Flex>
+          <Flex direction={"column"}>
+            <Input
+              fontSize="20px"
+              type="email"
+              placeholder="Email"
+              w={"40vh"}
+              h={"4vh"}
+              backgroundColor={"white"}
+              marginBottom={"20px"}
+              marginTop={"20px"}
+            />
+            <Input
+              fontSize="20px"
+              type="password"
+              placeholder="Password"
+              w={"40vh"}
+              h={"4vh"}
+              backgroundColor={"white"}
+              marginBottom={"20px"}
+            />
+          </Flex>
+        </FormControl>
+        <Text  fontSize={"12px"} marginBottom={"20px"} color={"white"} w={"40vh"}>
+          By clicking Sign Up, you agree to our Terms, Data Policy and Cookies Policy.
+          You may receive email from us and can opu out at any time.
+        </Text>
+        <Button
+          w={"18vh"}
+          h={"5.5vh"}
+          fontSize={"20px"}
+          backgroundColor={"#008F20"}
+          color={"White"}
+          _hover={{ bg: "#d6d6d6", color: "black" }}
+        >
+          Sign Up
+        </Button>
+      </Flex>
+    </Box>
+  );
+};
+export default Register;
