8fae9d743cb0fc98dcf62f4a850d581837c5f4dc
HelloSeaNation <sookann@gmail.com>
2024-05-23 15:09:36 +1200

    Notification page

--- src/components/NotificationContent.tsx
+++ src/components/NotificationContent.tsx
@@ -1,18 +1,30 @@
 import React from "react";
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Flex, Image, Text } from "@chakra-ui/react";
+import { AddIcon } from "@chakra-ui/icons";
+import { faImage } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const DashboardContent = () => {
   return (
     <>
       <Box
-        w={"70%"}
+        w={"65%"}
         margin={"auto"}
-        border={"2px solid #D4D4D4"}
+        border={"2px solid #E6E6E6"}
         borderRadius={"10"}
+        marginTop={"2rem"}
       >
-        <Flex>
-            
-            </Flex>
+        <Flex alignItems={"center"} padding={"1rem"}>
+          <FontAwesomeIcon icon={faImage} size="4x" width={"30%"} />
+          <Flex direction={"column"}>
+            <Text fontSize={"18px"}>
+              {"{client full name} "} is access the {" {gallery name} "}
+            </Text>
+            <Text color={"#4267CF"} fontSize={"14px"}>
+                7 hours ago
+            </Text>
+          </Flex>
+        </Flex>
       </Box>
     </>
   );
