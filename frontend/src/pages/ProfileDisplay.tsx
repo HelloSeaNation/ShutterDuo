4e96c545068f9b15d64e4b35e9686e79d010e9d1
Keshia-w <keshia.w196@gmail.com>
2024-06-06 10:15:26 +1200

    Profile page started and topbar drop down now includes users full name

--- frontend/src/pages/ProfileDisplay.tsx
+++ frontend/src/pages/ProfileDisplay.tsx
@@ -1,9 +1,145 @@
 import React, {useState, useEffect} from 'react'
+import {Flex, Text, Box} from '@chakra-ui/react'
+import TopBar from "../components/TopBar";
+import axios from 'axios'
+import { CalendarIcon, StarIcon} from '@chakra-ui/icons'
+
+
+
+
+interface User { //props for profile setup and editing
+  firstName: string;
+  surname: string;
+  email: string;
+  business: string;
+  bio?: string;
+  location?: string;
+  job?: string;
+  phone?: string;
+  // facebook?: string;
+  // insta?: string;
+  // pinterest?: string;
+  // twitter?: string;
+  // youtube?: string;
+  // linkedin?: string;
+  // tiktok?: string;
+}
+
+
+
 
 const ProfilePage = () => {
-    return(
-        <></>
-    )
+
+
+
+
+  const [user, setUser] = useState<User | null>(null);
+
+
+
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      const storedUser = localStorage.getItem('user');
+      if (storedUser) {
+        const parsedUser = JSON.parse(storedUser);
+        if (parsedUser && parsedUser.email) {
+          try {
+            const response = await axios.get(`http://localhost:5000/user/${parsedUser.email}`);
+            setUser(response.data);
+          } catch (error) {
+            console.error("Error fetching user data:", error);
+          }
+        }
+      }
+    };
+     fetchUserData();
+  }, []);
+
+
+
+
+  return(
+      <>
+          <TopBar />
+
+
+           {/* Basic information and profile photo */}
+          <Box padding={35}>
+
+
+              <Text marginBottom={2} fontWeight="bold">
+                   {user ? `${user.firstName} ${user.surname}` : 'Users'}
+              </Text>
+
+
+              <Box color="#949494">
+
+
+              <Text>
+              <CalendarIcon marginRight={2} boxSize={4}/>
+                  {user ? `${user.location}` : 'Users'}
+              </Text>
+
+
+              <Text>
+              <StarIcon marginRight={2} boxSize={4} />
+                  {user ? `${user.job}` : 'Users'}
+              </Text>
+
+
+          </Box>
+
+
+
+
+          {/* About me section */}
+          <Box marginTop={50} >
+
+
+
+
+          <Text marginBottom={3} fontWeight="bold"> About Me </Text>
+
+
+
+
+              <Box borderRadius={10}
+                  borderColor="#949494"
+                  borderWidth={1}        
+                  borderStyle="solid"
+                  padding={3}
+                  width={400}
+                  height={200}>
+                  {user ? `${user.bio}` : 'Users'}
+              </Box>
+          </Box>
+        
+          {/* Contact Info */}
+          <Box marginTop={50}>
+          <Text marginBottom={3} fontWeight="bold"> Contact Informtion </Text>
+
+
+
+
+          <Text>
+              Phone: {user ? `${user.phone}` : 'Users'}
+          </Text>
+
+
+
+
+          <Text>
+              Email: {user ? `${user.email}` : 'Users'}
+          </Text>
+          </Box>
+          </Box>
+        
+      </>
+  )
 }
+
+
+
 
 export default ProfilePage;
