c485c68fe1a6d52d66cbf5bda2e76535393bb697
HelloSeaNation <sookann@gmail.com>
2024-06-05 14:15:45 +1200

    Commit gallery page

--- frontend/src/components/GalleryContent.tsx
+++ frontend/src/components/GalleryContent.tsx
@@ -1,29 +1,97 @@
-import React from "react";
-import { Box, Flex, Text } from "@chakra-ui/react";
-import { faImage } from "@fortawesome/free-solid-svg-icons";
+import React, { useEffect, useState } from "react";
+import { Box, Flex, Text, Card, CardBody, Image } from "@chakra-ui/react";
+import { fetchGalleriesData, Gallery } from "./api";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEllipsis } from "@fortawesome/free-solid-svg-icons";
 
 const GalleryContent = () => {
+  const [galleries, setGalleries] = useState<Gallery[]>([]);
+
+  const handleCardClick = (galleryId: string) => {
+    window.location.href = `/gallery/${galleryId}`;
+  };
+
+  const loadGalleries = async () => {
+    try {
+      const data = await fetchGalleriesData();
+      setGalleries(data);
+    } catch (error) {
+      console.error("Error loading galleries:", error);
+    }
+  };
+
+  useEffect(() => {
+    loadGalleries();
+  }, []);
+
+  const getRandomImageUrl = () => {
+    const randomId = Math.floor(Math.random() * 1000) + 1;
+    return `https://picsum.photos/300/200?random=${randomId}`;
+  };
+
   return (
     <>
-      <Box
-        w={"90%"}
-        margin={"auto"}
-        border={"2px solid #E6E6E6"}
-        borderRadius={"10"}
-        marginTop={"2rem"}
-      >
+      <Box w={"90%"} margin={"auto"} marginTop={"2rem"}>
         <Flex
-          direction={"column"}
-          justifyContent={"center"}
-          alignItems={"center"}
+          direction={"row"}
           margin={"auto"}
           marginTop={"2rem"}
+          justifyContent={"space-evenly"}
         >
-          <FontAwesomeIcon icon={faImage} size="5x" color={"#E6E6E6"} />
-          <Text fontSize={"1.5rem"} color={"#E6E6E6"} marginTop={"1rem"}>
-            No images to display
-          </Text>
+          <Flex
+            overflowY="auto"
+            direction={"row"}
+            flexWrap={"wrap"}
+            justifyContent={"space-between"}
+          >
+            {galleries.map((gallery) => (
+              <Card
+                key={gallery._id}
+                onClick={() => handleCardClick(gallery._id)}
+                shadow={"none"}
+              >
+                <CardBody mb={10}>
+                  <Flex
+                    direction={"column"}
+                    alignItems={"flex-start"}
+                    justifyContent={"space-evenly"}
+                  >
+                    <Image
+                      src={getRandomImageUrl()}
+                      alt="Placeholder"
+                      style={{
+                        width: "20rem",
+                        height: "13rem",
+                        marginRight: "2rem",
+                      }}
+                    />
+                    <Flex direction={"column"} width={"90%"}>
+                      <Flex
+                        direction={"row"}
+                        marginTop="10px"
+                        marginBottom="10px"
+                        alignItems={"center"}
+                        justifyContent={"space-between"}
+                      >
+                        <Text fontSize="2opx" fontWeight="bold">
+                          {gallery.title}
+                        </Text>
+                        <FontAwesomeIcon
+                          icon={faEllipsis}
+                          color="#4267CF"
+                          size="xl"
+                        />
+                      </Flex>
+
+                      <Text fontSize="15px" color={"#9E9E9E"}>
+                        {gallery.description}
+                      </Text>
+                    </Flex>
+                  </Flex>
+                </CardBody>
+              </Card>
+            ))}
+          </Flex>
         </Flex>
       </Box>
     </>
